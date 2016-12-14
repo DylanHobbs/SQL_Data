bee936b9245f8245d974c1d7c0d90318aa3ffe6e
Dylan Hobbs <dhobbs@tcd.ie>
2016-12-14 03:19:12 +0000

    Adding SQL Data Inserts

---
+++ Player.csv.sql
@@ -0,0 +1,5 @@
+INSERT INTO PLAYER ("PLAYER_NO", "NAME", "GOLD", "SILVER", "COPPER", "LVL", "STR", "DEX", "WIS", "CON", "INTEL", "CHARISMA") VALUES ('1', 'WOOFRIN', '1000', '40', '10', '5', '17', '12', '8', '20', '10', '9');
+INSERT INTO PLAYER ("PLAYER_NO", "NAME", "GOLD", "SILVER", "COPPER", "LVL", "STR", "DEX", "WIS", "CON", "INTEL", "CHARISMA") VALUES ('2', 'DONALD', '2340', '30', '10', '5', '9', '15', '10', '14', '18', '15');
+INSERT INTO PLAYER ("PLAYER_NO", "NAME", "GOLD", "SILVER", "COPPER", "LVL", "STR", "DEX", "WIS", "CON", "INTEL", "CHARISMA") VALUES ('3', 'BREGOR', '10', '10', '0', '4', '10', '19', '10', '10', '14', '14');
+INSERT INTO PLAYER ("PLAYER_NO", "NAME", "GOLD", "SILVER", "COPPER", "LVL", "STR", "DEX", "WIS", "CON", "INTEL", "CHARISMA") VALUES ('4', 'JUDMISTER', '4000', '80', '40', '6', '10', '11', '20', '10', '20', '15');
+INSERT INTO PLAYER ("PLAYER_NO", "NAME", "GOLD", "SILVER", "COPPER", "LVL", "STR", "DEX", "WIS", "CON", "INTEL", "CHARISMA") VALUES ('5', 'SANDT', '100000', '0', '0', '20', '20', '20', '20', '20', '20', '20');
